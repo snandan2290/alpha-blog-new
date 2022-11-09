d432d5a6c67dd23023b36e26d84d03c97a8d103a
VnandanS <vidyanandan.s@moviuscorp.com>
2022-11-09 14:27:15 +0530

    Adding links for home, about, artile- index, show etc.etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,7 +43,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      # byebug
       @article  = Article.find(params[:id])
       @article.destroy
       redirect_to articles_path
