6620b1be1be4da76ebe2b35555f18be41ce77a2e
VnandanS <vidyanandan.s@moviuscorp.com>
2022-11-09 14:09:46 +0530

    Article Destroy action with Edit. show and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,4 +41,11 @@ class ArticlesController < ApplicationController
       end
 
     end
+
+    def destroy
+      # byebug
+      @article  = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
 end
