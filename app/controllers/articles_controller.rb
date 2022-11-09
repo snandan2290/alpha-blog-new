4a83412063aeb0ad6c7d62f0576ee1317285dae6
VnandanS <vidyanandan.s@moviuscorp.com>
2022-11-09 10:44:55 +0530

    New article create and save and render and render the article on successsfull creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,18 @@ class ArticlesController < ApplicationController
         @articles =Article.all
     end
 
+    def new
+
+    end
+
+    def create
+        # render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        res = @article.save
+        @articles = Article.all
+        # render plain: @articles.inspect
+        # render plain: @article.inspect
+        redirect_to  @article
+    end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
-    validates :description, presence: true, length: {minimum: 50, maximum: 255}
+    validates :description, presence: true, length: {minimum: 10, maximum: 255}
 end
