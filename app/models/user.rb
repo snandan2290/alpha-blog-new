f4f996bafab7ef742da3e202f58527f832ee4905
VnandanS <vidyanandan.s@moviuscorp.com>
2022-11-24 21:54:29 +0530

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
-    
+    validates :email, presence: true, 
+              uniqueness: true, 
+              length: { minimum: 9 },
+              format: { with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9].[a-z]+/, message: "Invalid Email address" }
+    validates :username, 
+              presence: true, 
+              uniqueness: true, 
+              length: { minimum: 6, maximum:22 }
+    validates :password, presence: true, length: {minimum:6, maximum:16}
 end
