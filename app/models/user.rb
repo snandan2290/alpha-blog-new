b3e668677b46b9204b32c7238fda62e7c12ba9e3
VnandanS <vidyanandan.s@moviuscorp.com>
2022-12-12 11:52:58 +0530

    installing new gem bcrypt for passwprd hasing and adding new field password_digest to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
 
-    before_save: {self.email = email.downcase}
+  has_secure_password
 
-    has_many :articles
+  before_save { self.email = email.downcase }
+
+  has_many :articles
 
 
-    validates :email, presence: true, 
-              uniqueness: true, 
-              length: { minimum: 9 },
-              format: { with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9].[a-z]+/, message: "Invalid Email address" }
-    validates :username, 
-              presence: true, 
-              uniqueness: true, 
-              length: { minimum: 6, maximum:22 }
-    validates :password, presence: true, length: {minimum:6, maximum:16}
+  validates :email, presence: true, 
+            uniqueness: true, 
+            length: { minimum: 9 },
+            format: { with: /[a-zA-Z0-9+_.-]+@[a-zA-Z0-9].[a-z]+/, message: "Invalid Email address" }
+  validates :username, 
+            presence: true, 
+            uniqueness: true, 
+            length: { minimum: 6, maximum:22 }
+  validates :password, presence: true, length: {minimum:6, maximum:16}
 end
