2561c258ddc25f3a67308fb41c4ffe1399b56317
VnandanS <vidyanandan.s@moviuscorp.com>
2023-03-07 13:13:09 +0530

    User aignup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,21 @@ class UsersController < ApplicationController
 
     def new
         @user = User.new
-
     end
 
+    def create
+        # byebug
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Alpha-blog #{@user.username}, you have successfully signup"
+            redirect_to  articles_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update]
 
-  get 'signup' => 'users#new'
+  get 'signup', to: 'users#new'
 
   # post 'users' => 'users#create'
   resources :users, except: [:new]
